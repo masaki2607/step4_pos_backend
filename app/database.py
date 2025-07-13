3565376e6125d815b9a83567808d735e84aa44cf
masa26 <be.ar.ma.2627@gmail.com>
2025-07-13 21:46:04 +0900

    SSL証明削除

--- app/database.py
+++ app/database.py
@@ -15,7 +15,7 @@ user = os.environ.get("USER")  # 例: tech0sql1@rdbs-step4-brazil-south
 password = os.environ.get("PASSWORD")
 host = os.environ.get("HOST")
 database = os.environ.get("DATABASE")
-ssl_ca = os.environ.get("SSL_CA")  # 例: C:/path/to/DigiCertGlobalRootCA.crt.pem
+# ssl_ca = os.environ.get("SSL_CA")  # 例: C:/path/to/DigiCertGlobalRootCA.crt.pem
 
 DATABASE_URL = (
     f"mysql+pymysql://{user}:{password}@{host}/{database}"
